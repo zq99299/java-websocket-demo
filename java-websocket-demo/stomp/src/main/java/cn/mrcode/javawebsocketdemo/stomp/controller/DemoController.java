fe1123d40f32b5386341d6f1d1a055926ce48569
zhuqiang <zhuqiang@tidebuy.net>
2017-11-08 17:10:47 +0800

    广播-公共新闻订阅

--- java-websocket-demo/stomp/src/main/java/cn/mrcode/javawebsocketdemo/stomp/controller/DemoController.java
+++ java-websocket-demo/stomp/src/main/java/cn/mrcode/javawebsocketdemo/stomp/controller/DemoController.java
@@ -1,8 +1,15 @@
 package cn.mrcode.javawebsocketdemo.stomp.controller;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author : zhuqiang
@@ -12,8 +19,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(produces = BaseController.CHARSET_UTF8_JSON)
 public class DemoController extends BaseController {
-    @RequestMapping("/test")
-    public String test() {
-        return JSON.toJSONString("hello");
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    {
+        new Thread(() -> {
+            mockPublicNews();
+        }).start();
+    }
+
+    /**
+     * 模拟公共新闻推送
+     */
+    public void mockPublicNews() {
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            List<String> list = new ArrayList<>();
+
+            // 模拟产生3条
+            list.add("清华大学16位学霸为一事现身PK 简历吓坏网友 - " + randomInt());
+            list.add("闹市区树上长出6斤多蘑菇 保安大叔炒着吃了(图) - " + randomInt());
+            list.add("女司机第2次上路撞死过路老人 教练被抓走 司法频道 - " + randomInt());
+
+
+            // 广播消息，注意记住这里的地址
+            template.convertAndSend("/topic/public_news", JSON.toJSONString(list));
+        }
+    }
+
+    private int randomInt() {
+        return RandomUtils.nextInt(100, 1000);
     }
 }
--- java-websocket-demo/stomp/src/main/java/cn/mrcode/javawebsocketdemo/stomp/ws/WebSocketConfig.java
+++ java-websocket-demo/stomp/src/main/java/cn/mrcode/javawebsocketdemo/stomp/ws/WebSocketConfig.java
@@ -2,6 +2,7 @@ package cn.mrcode.javawebsocketdemo.stomp.ws;
 
 import com.alibaba.fastjson.support.spring.FastjsonSockJsMessageCodec;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -23,4 +24,15 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 //                .setClientLibraryUrl("//cdn.jsdelivr.net/sockjs/1/sockjs.min.js");
         ;
     }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // 前端发送消息和主动访问对应的消息地址前缀
+        config.setApplicationDestinationPrefixes("/app");
+        // 开启消息代理
+        config.enableSimpleBroker("/topic", "/queue");
+        // 点对点支持前缀
+        config.setUserDestinationPrefix("/user/");
+    }
+
 }
